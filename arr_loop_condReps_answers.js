b34bed8a6c699e847b8f4c739e4174ce9ee16dbf
Jeff McDonald <jwmcdonald85@gmail.com>
2023-11-06 18:02:34 -0500

    Easy Going answered

---
+++ arr_loop_condReps_answers.js
@@ -0,0 +1,28 @@
+// ------------------------------------------------Part I: HTML & CSS------------------------------------------------ //
+
+// HTML Intro (Hyper Text Markup Language) 
+    // 4 - I feel pretty good about the material, but some extra review would help
+        // Tags
+        // Content/Attributes (is within the tags)
+        // Symantic is choosing tags based off function
+
+// HTML Elements
+    // 3 - I almost feel like I've got it
+
+// CSS Intro (Cascading Style Sheet)
+    // 4 - I feel pretty good about the material, but some extra review would help
+
+// CSS Basics
+    // 4 - I feel pretty good about the material, but some extra review would help
+
+
+// ------------------------------------------------Part II: JavaScript Reps------------------------------------------------ //
+
+// ---------------------- //
+// ***** Easy Going ***** //
+
+for (let i = 1; i <= 20; i++) {
+    console.log(i);
+}
+// ---------------------- //
+
